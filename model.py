bb01521c7296f5132f2a2fd7632a2548d2a0107d
taichnak <tnakamura493@gmail.com>
2024-11-09 09:19:22 +0900

    separate model and process

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 from lightgbm import early_stopping, log_evaluation
 from catboost import CatBoostClassifier, CatBoostRegressor, Pool
@@ -44,7 +43,7 @@ class Common_LGB_Modelling:
         test_pred_all = []
         for idx in range(0, len(test), batch_size):
             test_pred_batch = [
-                model.predict(test.iloc[idx : idx + batch_size]) for model in models
+                model.predict(test.iloc[idx: idx + batch_size]) for model in models
             ]
             test_pred_batch = np.mean(test_pred_batch, axis=0)
             test_pred_all.append(test_pred_batch)
@@ -54,7 +53,7 @@ class Common_LGB_Modelling:
         test_pred_all = []
         for idx in range(0, len(test), batch_size):
             test_pred_batch = [
-                model.predict(test[idx : idx + batch_size]) for model in models
+                model.predict(test[idx: idx + batch_size]) for model in models
             ]
             test_pred_batch = np.mean(test_pred_batch, axis=0)
             test_pred_all.append(test_pred_batch)
@@ -117,12 +116,12 @@ class Common_CB_Modelling:
         for idx in range(0, len(test), batch_size):
             if isinstance(models[0], CatBoostClassifier):
                 test_pred_batch = [
-                    model.predict_proba(test.iloc[idx : idx + batch_size])[:, 1]
+                    model.predict_proba(test.iloc[idx: idx + batch_size])[:, 1]
                     for model in models
                 ]
             elif isinstance(models[0], CatBoostRegressor):
                 test_pred_batch = [
-                    model.predict(test.iloc[idx : idx + batch_size]) for model in models
+                    model.predict(test.iloc[idx: idx + batch_size]) for model in models
                 ]
             test_pred_batch = np.mean(test_pred_batch, axis=0)
             test_pred_all.append(test_pred_batch)
@@ -133,12 +132,12 @@ class Common_CB_Modelling:
         for idx in range(0, len(test), batch_size):
             if isinstance(models[0], CatBoostClassifier):
                 test_pred_batch = [
-                    model.predict_proba(test[idx : idx + batch_size])[:, 1]
+                    model.predict_proba(test[idx: idx + batch_size])[:, 1]
                     for model in models
                 ]
             elif isinstance(models[0], CatBoostRegressor):
                 test_pred_batch = [
-                    model.predict(test[idx : idx + batch_size]) for model in models
+                    model.predict(test[idx: idx + batch_size]) for model in models
                 ]
             test_pred_batch = np.mean(test_pred_batch, axis=0)
             test_pred_all.append(test_pred_batch)
@@ -171,49 +170,3 @@ class Common_XGB_Modelling:
         ]
         test_pred = np.mean(test_pred, axis=0)
         return test_pred
-
-
-def reduce_mem_usage(df):
-    """iterate through all the columns of a dataframe and modify the data type
-    to reduce memory usage.
-    """
-    start_mem = df.memory_usage().sum() / 1024**2
-    print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
-
-    for col in df.columns:
-        col_type = df[col].dtype
-        if str(col_type) == "category":
-            continue
-
-        if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == "int":
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                if (
-                    c_min > np.finfo(np.float16).min
-                    and c_max < np.finfo(np.float16).max
-                ):
-                    df[col] = df[col].astype(np.float16)
-                elif (
-                    c_min > np.finfo(np.float32).min
-                    and c_max < np.finfo(np.float32).max
-                ):
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
-        else:
-            df[col] = df[col].astype("category")
-    end_mem = df.memory_usage().sum() / 1024**2
-    print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
-    print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
-
-    return df
